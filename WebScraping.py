4ef6491a3dde7b0891ca30a429434dca4b4ea2b1
Tayzerdo <64328213+Tayzerdo@users.noreply.github.com>
2021-01-09 23:09:49 -0300

    Add files via upload

--- WebScraping.py
+++ WebScraping.py
@@ -12,38 +12,61 @@ from bs4 import BeautifulSoup as BS
 import requests
 import json
 import pandas as pd
-print('libraries imported')
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+print("libraries imported")
 
 #Make the request
+html = requests.get("https://www.climatempo.com.br/previsao-do-tempo/agora/cidade/321/riodejaneiro-rj").content
+now = BS(html, "lxml")
+
 html = requests.get("https://www.climatempo.com.br/previsao-do-tempo/cidade/321/riodejaneiro-rj/").content
-soup = BS(html, 'lxml')
+today = BS(html, "lxml")
 
 
-#STEP 1 - Take the general forecast for the day
-info = soup.find_all("ul", class_="variables-list")[0]
-print('---------')
+#STEP 1 - Take the information for now
+timenow = datetime.now()
+print("The time for now is: ",timenow.hour, ':', timenow.minute)
+instant = list(now.find("div", class_="card _justify-center").text.split("\n"))
+instant= list(filter(None,instant))
+print(instant)
+print("---------")
+
+
+
+print("---------------------------")
+#STEP 2 - Take the general forecast for the day
+info = today.find_all("ul", class_="variables-list")[0]
+print("---------")
+
+##Check the date
+timestamp = today.find("h1",class_="-bold -font-18 -dark-blue _margin-r-10 _margin-b-sm-5").text
+print(timestamp)
+print("---------")
+
 ##Extract the minumum and maximum temperature
 tempMin = info.find(class_="_margin-r-20").text
 print(f'The minimum temperature is: {tempMin}')
 tempMax = info.find(id="max-temp-1").text
 print(f'The maximum temperature is: {tempMax}')
-print('---------')
+print("---------")
 
 ##Extract the rainy in millimeters and the probability
-rainy = info.find("span",class_="_margin-l-5").text.replace(' ','').split("-")
+rainy = info.find("span",class_="_margin-l-5").text.replace(" ","").split("-")
 rainyMil = rainy[0]
 rainyProb = rainy[1]
 print(f'The rainy is: {rainyMil}')
 print(f'The probability to rain is: {rainyProb}')
-print('---------')
+print("---------")
 
 ##Extract the information for the wind direction and the velocity in km/h
-wind = info.find_all(class_="item")[2].div.text.replace(' ','').strip().split("-")
+wind = info.find_all(class_="item")[2].div.text.replace(" ","").strip().split("-")
 windDirec = wind[0]
 windVelo = wind[1]
 print(f'The wind velocity will be: {windVelo}')
 print(f'The wind direction will be: {windDirec}')
-print('---------')
+print("---------")
 
 humidity = info.find_all(class_="item")[3].div
 hum = humidity.find_all("span")
@@ -52,7 +75,7 @@ humMax = hum[3].text
 print(f'The minimum humidity is: {humMin}')
 print(f'The maximum humidity is: {humMax}')
 
-print('---------')
+print("---------")
 
 sun = info.find_all(class_="item")[4]
 sunny = sun.find_all("span")
@@ -61,30 +84,66 @@ sunrise = suntime[0]
 sunset = suntime[1]
 print(f'The sunrise it will be at: {sunrise}')
 print(f'The sunset it will be at: {sunset}')
-print('---------')
 
-#STEP 2 - Take the hourly forecast
-hourly = soup.find("div",class_="card -no-top")
+
+print("---------------------------")
+#STEP 3 - Take the hourly forecast
+hourly = today.find("div",class_="card -no-top")
 forecast = hourly.find(class_="wrapper-chart")["data-infos"]
 forecastDict = json.loads(forecast)
-tableInfo = pd.DataFrame(columns=['Date', 'Hour','Humidity','Precipitation','Temperature','WindVelo','WindDirec'])
+tableInfo = pd.DataFrame(columns=["Date", "Hour","Humidity","Precipitation","Temperature","Wind","WindDirec"])
 for i in forecastDict:
-    tableInfo = tableInfo.append({'Date':i["date"].split()[0],
-                                  'Hour':i["date"].split()[1].split(":")[0],
-                                  'WindVelo':i["wind"]["velocity"],
-                                  'WindDirec':i["wind"]["direction"],
-                                  'Temperature':i["temperature"]["temperature"],
-                                  'Precipitation':i["rain"]["precipitation"],
-                                  'Humidity':i["humidity"]["relativeHumidity"]},ignore_index=True)
+    tableInfo = tableInfo.append({"Date":i["date"].split()[0],
+                                  "Hour":i["date"].split()[1].split(":")[0],
+                                  "Wind":i["wind"]["velocity"],
+                                  "WindDirec":i["wind"]["direction"],
+                                  "Temperature":i["temperature"]["temperature"],
+                                  "Precipitation":i["rain"]["precipitation"],
+                                  "Humidity":i["humidity"]["relativeHumidity"]},ignore_index=True)
     # print(f'The wind velocity will be: {i["wind"]["velocity"]}')
     # print(f'The wind direction will be: {i["wind"]["direction"]}')
     # print(f'The temperature will be: {i["temperature"]["temperature"]}')
     # print(f'The precipitation will be: {i["rain"]["precipitation"]}')
     # print(f'The relative humidity will be: {i["humidity"]["relativeHumidity"]}')
-    print('---')
-
-print(tableInfo)
+    # print("---")
 
 
+tableInfo[["humMin","humMax","rainyMil","tempMin","tempMax","windVelo"]]=[humMin[:2],humMax[:2],rainyMil[:-2],tempMin[:-1],tempMax[:-1],windVelo[:-4]]
 
+##Change the data type
+tableInfo[["Hour","Humidity","Precipitation","Temperature","Wind","humMin","humMax","rainyMil","tempMin","tempMax",
+            "windVelo"]] = tableInfo[["Hour",
+        "Humidity","Precipitation","Temperature","Wind","humMin","humMax","rainyMil","tempMin","tempMax","windVelo"]].apply(pd.to_numeric)
+tableInfo["PrecipitationAccumulative"] = tableInfo["Precipitation"].cumsum()
 
+print(tableInfo.dtypes)
+print(tableInfo.head(0))
+
+##Save the table in excel file
+tableInfo.to_excel("Table info.xlsx")
+
+# ##Plot the graphs
+# plt.plot(tableInfo["Hour"], tableInfo["Humidity"], label="forecast", color="black")
+# plt.plot(tableInfo["Hour"], tableInfo["humMin"], label="humMin", color="Lightblue", linestyle="dashed")
+# plt.plot(tableInfo["Hour"], tableInfo["humMax"], label="humMax", color="Lightgreen", linestyle="dashed")
+# plt.title("Forecast Humidity")
+# plt.xlabel("Hour")
+# plt.ylabel("Humidity (%)")
+# plt.xlim(xmin=0,xmax=23)
+# plt.legend()
+# plt.show()
+
+# plt.plot(tableInfo["Hour"], tableInfo["Temperature"], label="Temperature", color="black")
+# plt.plot(tableInfo["Hour"], tableInfo["tempMin"], label="tempMin", color="Lightblue", linestyle="dashed")
+# plt.plot(tableInfo["Hour"], tableInfo["tempMax"], label="tempMax", color="Lightgreen", linestyle="dashed")
+# plt.title("Forecast Temperature")
+# plt.xlabel("Hour")
+# plt.ylabel("Temperature (°)")
+# plt.xlim(xmin=0,xmax=23)
+# plt.legend(loc=1)
+# plt.show()
+
+# plt.bar(tableInfo["Hour"], tableInfo["Precipitation"])
+# #plt.bar(tableInfo["Hour"], tableInfo["PrecipitationAccumulative"])
+# plt.xlim(xmin=0,xmax=23)
+# plt.show()
